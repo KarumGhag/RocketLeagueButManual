ab299fd01ace46ca490134fc483510ac7079f580
Karum Ghag <karumghag09@gmail.com>
2026-09-13 21:40:47 +0100

    connected to port

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Sockets;
+using System.Text.Json;
+using System.Text;
+
+const string host = "127.0.0.1";
+const int port = 49123;
+
+using TcpClient client = new TcpClient();
+client.Connect(host, port);
