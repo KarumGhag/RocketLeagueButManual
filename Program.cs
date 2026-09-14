d40b8f73950c0af5b66fd13a8ad1d20e9c5fd3e1
Karum Ghag <karumghag09@gmail.com>
2026-09-14 18:02:04 +0100

    moved speed extractor to its own file and added mph conversion

--- Program.cs
+++ Program.cs
@@ -1,61 +1,23 @@
-﻿using System.Net.Sockets;
-using System.Text.Json;
-using System.Text;
+﻿using Speed.Extractor;
 
-const string host = "127.0.0.1";
-const int port = 49123;
+SpeedExtractor speedExtractor = new SpeedExtractor();
 
-// Create client and connect to port
-using TcpClient client = new TcpClient(host, port);
-NetworkStream stream = client.GetStream();
-
-// this is where all the data will be
-byte[] buffer = new byte[16384];
+// Conversion factor from km/h to MPH
+const double KmhToMphFactor = 0.621371;
 
 while (true)
 {
-    // populate the buffer with data by overriding, items 0 to the amount of data read, of the buffer with new data
-    // returns how much data was written
-    int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
-    // if no data then skip
-    if (bytesRead == 0) continue;
+    // Speed received directly from the API in km/h
+    double kmhSpeed = await speedExtractor.GetSpeed();
 
-    // converts bytes to string
-    // gets the bytes sat between item 0 and bytesread of the buffer (all the data that was just overriden)
-    string incoming = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-
-    try
+    if (kmhSpeed == -1)
     {
-        // parses everything
-        using JsonDocument all = JsonDocument.Parse(incoming);
-
-        // tries to find the data part, sets it to data element
-        all.RootElement.TryGetProperty("Data", out JsonElement dataElement);
-
-        // converts the element to a string to re parse
-        string? dataElementToParse = dataElement.GetString();
-        // if empty then skip
-        if (dataElementToParse == null) continue;
-
-        using JsonDocument data = JsonDocument.Parse(dataElementToParse);
-        // gets the players part of data
-        data.RootElement.TryGetProperty("Players", out JsonElement players);
-
-        // if no players then skip
-        if (players.GetArrayLength() == 0) continue;
-        // the first player (will have to change to find a certain player)
-        JsonElement player = players[0];
-
-        // try and get the property, if below is true then there is no speed in this stream and it should continue
-        // otherwise it sets speed element to the speed element
-        if (!player.TryGetProperty("Speed", out JsonElement speedElement)) continue;
-
-        // read the speed
-        double speed = speedElement.GetDouble();
-        Console.WriteLine($"Speed: {speed}");
-    }
-    catch
-    {
+        Console.WriteLine("failed");
         continue;
     }
+
+    // Convert km/h to MPH
+    double mphSpeed = kmhSpeed * KmhToMphFactor;
+
+    Console.WriteLine($"Speed: {mphSpeed:F1} MPH ({kmhSpeed:F1} km/h)");
 }
